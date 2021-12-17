792f638901bbecc6bc35123e62f6114cbad468d7
Dmitriy Yakovenko <jads84@gmail.com>
2021-12-17 18:02:01 +0300

    add form players/add-new

--- src/main/java/com/wutreg/flamehw/controller/PlayerController.java
+++ src/main/java/com/wutreg/flamehw/controller/PlayerController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("players")
 public class PlayerController {
 
     private final PlayerRepository repository;
@@ -19,8 +21,14 @@ public class PlayerController {
 
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("something", "this is coming from the controller");
+        model.addAttribute("something", "All players in Data Base");
         model.addAttribute("players", repository.findAll());
-        return "players";
+        return "players/show-all";
+    }
+
+    @GetMapping("add")
+    public String showAddForm(Model model) {
+        model.addAttribute("something", "Add player in Data Base");
+        return "players/add-new";
     }
 }
